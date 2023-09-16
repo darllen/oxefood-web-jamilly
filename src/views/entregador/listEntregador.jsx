abe7f8c3f5a8d871b5d55a2c80e2d321d3ca143f
Aluno <aluno@ifpe>
2023-09-16 10:22:06 -0300

    crud completo da entidade Entregador

--- src/views/entregador/listEntregador.jsx
+++ src/views/entregador/listEntregador.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Button, Container, Divider, Icon, Table } from 'semantic-ui-react';
+import { Button, Container, Divider, Icon, Table, Modal, Header } from 'semantic-ui-react';
 import MenuSistema from '../../menuSistema';
 
 
@@ -10,6 +10,9 @@ export default function ListEntregador() {
     const ENDERECO_API = 'http://localhost:8080/api/entregador/';
 
     const [lista, setLista] = useState([]);
+    const [openModal, setOpenModal] = useState(false);
+    const [idRemover, setIdRemover] = useState();
+
 
     useEffect(() => {
         carregarLista();
@@ -33,6 +36,31 @@ export default function ListEntregador() {
         return arrayData[2] + '/' + arrayData[1] + '/' + arrayData[0];
 
     }
+
+    function confirmaRemover(id) {
+        setOpenModal(true)
+        setIdRemover(id)
+    }
+
+    async function remover() {
+
+        await axios.delete(ENDERECO_API + idRemover)
+        .then((response) => {
+  
+            console.log('Entregador removido com sucesso.')
+  
+            axios.get(ENDERECO_API)
+            .then((response) => {
+                setLista(response.data)
+            })
+        })
+        .catch((error) => {
+            console.log('Erro ao remover um entregador.')
+        })
+        setOpenModal(false)
+    }
+ 
+ 
 
     return (
         <div>
@@ -94,7 +122,8 @@ export default function ListEntregador() {
                                                 circular
                                                 color='red'
                                                 title='Clique aqui para remover este entregador'
-                                                icon>
+                                                icon
+                                                onClick={e => confirmaRemover(entregador.id)}>
                                                 <Icon name='trash' />
                                             </Button>
 
@@ -109,6 +138,26 @@ export default function ListEntregador() {
                     </div>
                 </Container>
             </div>
+
+            <Modal
+               basic
+               onClose={() => setOpenModal(false)}
+               onOpen={() => setOpenModal(true)}
+               open={openModal}
+         >
+               <Header icon>
+                   <Icon name='trash' />
+                   <div style={{marginTop: '5%'}}> Tem certeza que deseja remover esse registro? </div>
+               </Header>
+               <Modal.Actions>
+                   <Button basic color='red' inverted onClick={() => setOpenModal(false)}>
+                       <Icon name='remove' /> Não
+                   </Button>
+                   <Button color='green' inverted onClick={() => remover()}>
+                       <Icon name='checkmark' /> Sim
+                   </Button>
+               </Modal.Actions>
+         </Modal>
 
         </div>
     )
